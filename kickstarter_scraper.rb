867767f8dc457b541bc23e02ae34362fc70a3149
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-04-17 07:52:49 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,9 @@
-# require libraries/modules here
+require 'pry'
+require 'nokogiri'
 
 def create_project_hash
   # write your code here
-end
-
+end 
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
